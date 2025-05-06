cae0332e13c2bb9d3c20d34af1ff35e7bb0a88cc
Sammyzain48 <samuelnwachukwu720@gmail.com>
2025-05-06 19:00:02 +0100

    update

---
+++ flixx-app/js/script.js
@@ -0,0 +1,87 @@
+const global = {
+  currentPage: window.location.pathname
+}
+
+
+const showActiveLink = () => {
+  const allLinks = document.querySelectorAll(".nav-link");
+  allLinks.forEach((link) => {
+    if(link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  })
+}
+
+const fetchData = async (endpoint) => {
+  const url = "https://api.themoviedb.org/3/";
+  const API_KEY = "c44bc25193aa9b19ea9441b23874ecb2";
+  const language = "en-US";
+  
+  const request = await fetch(`${url}${endpoint}?api_key=${API_KEY}&language=${language}`);
+  const response = await request.json();
+  const data = await response;
+  return data;
+}
+
+const showPopularMovies = async () => {
+  const { results } = await fetchData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="movie-details.html?id=1">
+            ${
+            movie.poster_path ? 
+            `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+            :
+            `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+              
+            }
+          </a>
+          
+      <div class = "card-body" >
+        <h5 class="card-title">${movie.title}</h5> 
+        <p class = "card-text" >
+        <small class="text-muted">Release:${movie.release_date}</small></p> 
+      </div>
+    `
+    document.querySelector("#popular-movies").appendChild(div);
+  })
+}
+const init = () => {
+  switch(global.currentPage) {
+    case "/flixx-app/" : 
+      case "/flixx-app/index.html" :
+        showPopularMovies();
+    break;
+    
+    case "/flixx-app/movie-details.html" :
+      console.log("Movie");
+    break;
+    
+    case "/flixx-app/tv-details.html" :
+      console.log("Tv Shows");
+    break;
+    
+    case "/flixx-app/search.html" :
+      console.log("Search");
+    break;
+    
+    case "/flixx-app/shows.html" : 
+      console.log("Shows");
+    break;
+  }
+  
+  showActiveLink();
+}
+
+
+document.addEventListener("DOMContentLoaded", init);
