81c7734104e9c4feca3059bafac2a8764ee82771
Sammyzain48 <samuelnwachukwu720@gmail.com>
2025-05-18 22:50:52 +0100

    update

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -30,7 +30,7 @@ const showPopularMovies = async () => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
-      <a href="movie-details.html?id={movie.id}">
+      <a href="movie-details.html?id=${movie.id}">
             ${
             movie.poster_path ? 
             `<img
@@ -95,9 +95,100 @@ const showPopularShow = async () => {
 
 //movie details
 const movieDetails = async () => {
-  console.log(window.location.search);
+  const movieID = window.location.search.split("=")[1];
+  const movie = await fetchData(`movie/${movieID}`);
+  
+  displayBackground("movie", movie.poster_path);
+  const div = document.createElement("div");
+    
+  div.innerHTML = `
+              <div class="details-top">
+          <div>
+${
+            movie.poster_path ? 
+            `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+            :
+            `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+              
+            }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary">${movie.vote_average.toFixed(1)} / 10</i>
+              
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => {
+               return `<li>${genre.name}</li>`
+              }).join("")}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span> $${movie.revenue}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((company) => {
+              return `<span>${company.name}</span>`
+            }).join("")}
+          </div>
+        </div>
+    `
+  document.querySelector("#movie-details").appendChild(div);
 }
-movieDetails();
+
+const showTvDetails = async () => {
+  const showsID = window.location.search.split("=")[1];
+  const movie = await fetchData(`movie/${showsID}`);
+
+  displayBackground("shows", shows.poster_path);
+  const div = document.createElement("div");
+  
+  document.querySelector("#show-details").appendChild(div);
+}
+
+const displayBackground = (type, backgroundPath) => {
+  const overlay = document.createElement("div");
+  overlay.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlay.style.backgroundSize = "cover";
+  overlay.style.backgroundPosition = "center";
+  overlay.style.backgroundRepeat = "no-repeat";
+  overlay.style.width = "100vw";
+  overlay.style.height = "100vh";
+  overlay.style.position = "absolute";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.zIndex = "-1";
+  overlay.style.opacity = "0.1";
+  
+  
+  if(type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlay);
+  }
+  else {
+    document.querySelector("#-details").appendChild(overlay);
+  }
+}
 
 
 const showSpinner = () => {
@@ -116,10 +207,11 @@ const init = () => {
     break;
     
     case "/flixx-app/movie-details.html" :
-      console.log("Movie");
+    movieDetails();
     break;
     
     case "/flixx-app/tv-details.html" :
+    showTvDetails();
     break;
     
     case "/flixx-app/search.html" :
